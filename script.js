6546656afe2855767b1044ef76384634735409ec
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-06-14 21:02:09 -0500

    graded assignment complete (rev 0)

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
-
 window.addEventListener("load", function() {
    this.document.addEventListener("submit", function(event) {
       event.preventDefault();
@@ -26,45 +12,52 @@ window.addEventListener("load", function() {
       } else if(isNaN(Number(level)) || isNaN(Number(mass))) {
          alert("Please provide numerical values for the 'Fuel Level' and 'Cargo Mass' fields.");
       } else {
-         // let css = document.styleSheets[0];
-         // let rules = css.cssRules;
-         // let visibilityIndex = -1;
-         // for(let i = 0; i < rules.length; i++) {
-         //    if(rules[i]==="#faultyItems") {
-         //       visibilityIndex = i;
-         //       alert(visibilityIndex);
-         //    }
-         // }
-         document.getElementById("pilotStatus").innerHTML = `Pilot ${name1} is ready for launch`;
-         document.getElementById("copilotStatus").innerHTML = `Co-pilot ${name2} is ready for launch`;
+         let pilotStatus = document.getElementById("pilotStatus");
+         pilotStatus.style.color = "green";
+         pilotStatus.innerHTML = `Pilot ${name1} is ready for launch`;
+         let copilotStatus = document.getElementById("copilotStatus");
+         copilotStatus.style.color = "green";
+         copilotStatus.innerHTML = `Co-pilot ${name2} is ready for launch`;
          let status = document.getElementById("launchStatus");
          if(Number(level)<10000) {
             status.innerHTML = "Shuttle not ready for launch";
             status.style.color = "red";
-            document.getElementById("fuelStatus").innerHTML = "Not enough fuel for launch";
+            document.getElementById("missionTarget").style.visibility = "hidden";
+            document.getElementById("faultyItems").style.visibility = "visible";
+            let fuelStatus = document.getElementById("fuelStatus");
+            fuelStatus.style.color = "red";
+            fuelStatus.innerHTML = "Not enough fuel for launch";
+         } else {
+            fuelStatus.style.color = "green";
          }
          if(Number(mass)>10000) {
             status.innerHTML = "Shuttle not ready for launch";
             status.style.color = "red";
-            document.getElementById("cargoStatus").innerHTML = "Cargo too heavy for launch";
+            document.getElementById("missionTarget").style.visibility = "hidden";
+            document.getElementById("faultyItems").style.visibility = "visible";
+            let cargoStatus = document.getElementById("cargoStatus");
+            cargoStatus.style.color = "red";
+            cargoStatus.innerHTML = "Cargo too heavy for launch";
+         } else {
+            cargoStatus.style.color = "green";
          }
          if(Number(level)>10000 && Number(mass)<10000) {
             status.innerHTML = "Shuttle is ready for launch";
             status.style.color = "green";
-            document.getElementById("fuelStatus").innerHTML = "Sufficient fuel for launch";
-            document.getElementById("cargoStatus").innerHTML = "Cargo light enough for launch";
+            document.getElementById("missionTarget").style.visibility = "visible";
+            document.getElementById("faultyItems").style.visibility = "hidden";
             fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
                response.json().then(function(json) {
-                  let planet = Math.floor(Math.random())*json.length;
-                  document.getElementById("missionTarget").innerHTML += `
+                  let planet = Math.floor(Math.random()*json.length);
+                  document.getElementById("missionTarget").innerHTML = `
                      <h2>Mission Destination</h2>
-                        <ol>
-                           <li>Name: ${json[planet].name}</li>
-                           <li>Diameter: ${json[planet].diameter}</li>
-                           <li>Star: ${json[planet].star}</li>
-                           <li>Distance from Earth: ${json[planet].distance}</li>
-                           <li>Number of Moons: ${json[planet].moons}</li>
-                        </ol>
+                     <ol>
+                        <li>Name: ${json[planet].name}</li>
+                        <li>Diameter: ${json[planet].diameter}</li>
+                        <li>Star: ${json[planet].star}</li>
+                        <li>Distance from Earth: ${json[planet].distance}</li>
+                        <li>Number of Moons: ${json[planet].moons}</li>
+                     </ol>
                      <img src="${json[planet].image}">
                   `
                });
